7ed7e08666821b7c7e5f8f5a6bcd10950d513d17
Victor Bona <victor.bona@hotmail.com>
2020-12-10 14:46:36 +0000

    Created other methods os readibility.

--- pylexitext/plots.py
+++ pylexitext/plots.py
@@ -2,11 +2,10 @@ from matplotlib import pyplot as plt
 from wordcloud import WordCloud
 
 def cloud_word(stopwords, text):
-    wordcloud = WordCloud(stopwords=stopwords).generate(text)
-
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-    plt.show()
+  wordcloud = WordCloud(stopwords=stopwords).generate(text)
+  plt.imshow(wordcloud, interpolation='bilinear')
+  plt.axis("off")
+  plt.show()
 
 def word_frequency_plot(fdist):
   fdist.plot(25, cumulative=False)
