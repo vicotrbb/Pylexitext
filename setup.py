a453fd3804883e4efe28dfd9d6ace5a90462bd3f
Victor Bona <victor.bona@hotmail.com>
2020-12-16 22:20:26 +0000

    init setup

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import find_packages, setup
 
+setup(
+    name='pylexitext',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Pylexitext is a python library that aggregates a series of NLP methods, text analysis, content converters and other usefull stuff.',
+    author='Victor Bona',
+    license='MIT',
+)
