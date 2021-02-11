b9a5f36f26f97e76ea1d6b4eceebca1bdd0c1161
Victor Bona <victor.bona@hotmail.com>
2021-02-11 20:51:02 -0300

    created sound to text convertor

--- pylexitext/text_converter.py
+++ pylexitext/text_converter.py
@@ -1,8 +1,53 @@
 from __future__ import unicode_literals
 import youtube_dl as yt
+import speech_recognition as sr
+import os
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import shutil
+
+from . import text
 
 
-def from_video_to_text(video_url, source='youtube'):
+def convert_sound_to_text(sound_file) -> text.Text:
+    """
+        Converts an audio to text
+    """
+
+    r = sr.Recognizer()
+    folder_name = "audio-chunks"
+    sound = AudioSegment.from_mp3(sound_file)
+
+    chunks = split_on_silence(
+        sound,
+        min_silence_len=500,
+        silence_thresh=sound.dBFS-14,
+        keep_silence=500,
+    )
+
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    converted_text = ""
+
+    for i, audio_chunk in enumerate(chunks, start=1):
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            try:
+                text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                print(chunk_filename, ":", text)
+                converted_text += text
+
+    shutil.rmtree(folder_name, ignore_errors=True)
+    return text.Text(converted_text)
+
+
+def from_video_to_text(video_path, source='youtube') -> text.Text:
     """
     Converts the audio of a video to text
     """
@@ -18,21 +63,16 @@ def from_video_to_text(video_url, source='youtube'):
         }
 
         output_file_name = None
-        extracted_info = None
         with yt.YoutubeDL(ydl_opts) as ydl:
-            extracted_info = ydl.extract_info(url=video_url, download=False)
+            extracted_info = ydl.extract_info(url=video_path, download=False)
             output_file_name = ydl.prepare_filename(extracted_info)
-            ydl.download([video_url])
+            ydl.download([video_path])
 
-        return output_file_name, extracted_info
+        return convert_sound_to_text(output_file_name)
 
-    else:
+    elif source == 'file':
         return
 
 
-def from_audio_to_text(audio, source='path'):
-    pass
-
-
-def from_podcast_to_text(url, source='spotfy'):
+def from_podcast_to_text(url, source='spotfy') -> text.Text:
     pass
